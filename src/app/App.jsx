9d74df459ce153f867186f96288c90d41219ec85
jc <jotace422@gmail.com>
2022-02-19 09:16:17 +0900

    Feat: 레이아웃 구현(네비게이션바, 사이드바)

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+import Navbar from '../components/navbar/Navbar.jsx';
+import Sidebar from '../components/sidebar/Sidebar.jsx';
 
 const App = () => {
-  return <div>Hey this is react Project</div>;
+  const [isDark, setIsDark] = useState(false);
+  return (
+    <>
+      <Navbar isDark={isDark} setIsDark={setIsDark} />
+      <Container>
+        <Sidebar />
+        <Content></Content>
+      </Container>
+    </>
+  );
 };
 
 export default App;
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Content = styled.div``;
