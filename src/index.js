f5fcd48469066756cea501a88d9639b19491cd06
jc <jotace422@gmail.com>
2022-02-19 22:57:26 +0900

    Chore: axios baseURL 설정

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 import App from './app/App.jsx';
+
+axios.defaults.baseURL = process.env.REACT_APP_SERVER_URL;
 
 ReactDOM.render(<App />, document.getElementById('root'));
