665ce1cc6d9b09d8d21cb51b3d5aa1e91bf6670b
jc <jotace422@gmail.com>
2022-02-19 22:54:16 +0900

    Feat: 환자 목록을 렌더하는 컴포넌트 구현

--- src/components/table/PatientList.jsx
+++ src/components/table/PatientList.jsx
@@ -1,7 +1,84 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const PatientList = () => {
-  return <div></div>;
+import PatientItem from './PatientItem.jsx';
+import Pagination from './Pagination.jsx';
+import Select from './Select.jsx';
+
+const PatientList = (props) => {
+  const { rows, totalRows, page, setPage, length, setLength, raceList, ethnicityList } = props;
+  /*
+  page: 1,
+  length: 10,
+  order_column: null, // person_id, gender, birth, race, ethnicity, death
+  order_desc: false,
+  gender: null,
+  race: null,
+  ethnicity: null,
+  age_min: null,
+  age_max: null,
+  death: null,
+*/
+
+  return (
+    <Wrapper>
+      <Head>
+        <PersonID>ID</PersonID>
+        <Select flex={1} {...props} text="Gender" options={['M', 'F']} />
+        <BirthDate>Birth date</BirthDate>
+        <Age>Age</Age>
+        <Select flex={2} {...props} text="Race" options={raceList} />
+        <Select flex={2} {...props} text="Ethnicity" options={ethnicityList} />
+        <Select flex={1} {...props} text="Death" options={['O', 'X']} />
+      </Head>
+      <Body>
+        {rows.map((rowData, idx) => (
+          <PatientItem key={idx} data={rowData} />
+        ))}
+      </Body>
+      <Pagination
+        page={page}
+        setPage={setPage}
+        length={length}
+        setLength={setLength}
+        totalRows={totalRows}
+        totalPages={Math.ceil(totalRows / length)}
+      />
+    </Wrapper>
+  );
 };
 
 export default PatientList;
+
+const Wrapper = styled.div`
+  font-size: 20px;
+`;
+
+const Head = styled.div`
+  display: flex;
+  & > * {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 10px 0;
+    border-bottom: 1px solid gray;
+  }
+`;
+
+const PersonID = styled.div`
+  flex: 2;
+`;
+
+const BirthDate = styled.div`
+  flex: 3;
+`;
+
+const Age = styled.div`
+  flex: 1;
+`;
+
+const Body = styled.div`
+  height: calc(80vh - 62px - 50px);
+  overflow-y: scroll;
+`;
